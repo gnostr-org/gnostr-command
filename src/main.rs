67cf341135474e31c01ae777313c6570c01e435b
randymcmillan <randy.lee.mcmillan@gmail.com>
2023-07-28 15:52:40 -0400

    intermediate

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,74 @@
 use std::env;
 use std::process;
+use std::io::{Result};
+use std::path::PathBuf;
+
+//time functions
+extern crate time;
+extern crate chrono;
+use chrono::{DateTime,Utc};
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use std::thread::sleep;
+
+//shell commands
+use std::process::Command;
+
+//lib.rs
 use gnostr_command::Config;
-fn main() {
+
+//main.rs functions
+fn get_epoch_ms() -> u128 {
+    SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_millis()
+}
+fn get_current_working_dir() -> std::io::Result<PathBuf> {
+    env::current_dir()
+}
+
+//debug
+#[cfg(debug_assertions)]
+
+fn example() {
+
+    //println!("Debugging enabled");
+    //println!("cwd={:?}",get_current_working_dir());
+
+}
+#[cfg(not(debug_assertions))]
+fn example() {
+
+    //println!("Debugging disabled");
+    //println!("cwd={:?}",get_current_working_dir());
+
+}
+
+fn main() -> Result<()> {
+
+    let start = time::get_time();
+    let epoch = get_epoch_ms();
+    let system_time = SystemTime::now();
+    let datetime: DateTime<Utc> = system_time.into();
 
     let args: Vec<String> = env::args().collect();
-
     let dirname = &args[0];
+
+    if cfg!(debug_assertions) {
+        //println!("Debugging enabled");
+    } else {
+        //println!("Debugging disabled");
+    }
+
+    #[cfg(debug_assertions)]
+    //println!("Debugging enabled");
+
+    #[cfg(not(debug_assertions))]
+    //println!("Debugging disabled");
+
+    example();
+
+
     let config = Config::build(&args).unwrap_or_else(|_err| {
         println!("Usage: gnostr-command <string> <file>");
         process::exit(1);
@@ -19,5 +82,8 @@ fn main() {
         println!("Application error: {e}");
         process::exit(1);
     }
+
+    let duration = time::get_time() - start;
+    Ok(())
 
 }
