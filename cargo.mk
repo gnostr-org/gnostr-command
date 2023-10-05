552d0ccc1351964110b7b514013fb1526ccdae3e
randymcmillan <randy.lee.mcmillan@gmail.com>
2023-10-05 07:55:09 -0400

    make: help formatting

--- cargo.mk
+++ cargo.mk
@@ -2,8 +2,7 @@
 ##make cargo-*
 cargo-help:### 	cargo-help
 	@awk 'BEGIN {FS = ":.*?###"} /^[a-zA-Z_-]+:.*?###/ {printf "\033[36m%-15s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
-cargo-b:cargo-build###	cargo b
-cargo-build:###	cargo build
+cargo-build:### 	cargo build
 ## 	make cargo-build q=true
 	@. $(HOME)/.cargo/env
 	@RUST_BACKTRACE=all cargo b $(QUIET)
@@ -11,24 +10,30 @@ cargo-install:### 	cargo install --path .
 #@. $(HOME)/.cargo/env
 	#@cargo install --path $(PWD)
 	@cargo install --locked --path $(PWD)
-cargo-br:cargo-build-release###	cargo-br
+cargo-br:cargo-build-release### 	cargo-br
 ## 	make cargo-br q=true
-cargo-build-release:###	cargo-build-release
+cargo-build-release:### 	cargo-build-release
 ## 	make cargo-build-release q=true
 	@. $(HOME)/.cargo/env
 	@cargo b --release $(QUIET)
-cargo-check:###	cargo-check
+cargo-check:### 	cargo-check
+## cargo c
 	@. $(HOME)/.cargo/env
 	@cargo c
-cargo-bench:###	cargo-bench
+cargo-bench:### 	cargo-bench
+## cargo b
+##	cargo build --release
 	@. $(HOME)/.cargo/env
 	@cargo bench
-cargo-test:###	cargo-test
+cargo-test:### 	cargo-test
+## cargo t
 	@. $(HOME)/.cargo/env
 	@cargo test
-cargo-report:###	cargo-report
+cargo-report:### 	cargo-report
 	@. $(HOME)/.cargo/env
 	cargo report future-incompatibilities --id 1
+cargo-doc:### 	cargo-doc
+	@cargo doc --no-deps --open
 
 # vim: set noexpandtab:
 # vim: set setfiletype make
