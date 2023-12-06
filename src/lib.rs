c1756adaa0b2e2a168e500cb56d1b25e4e966a23
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-12-05 23:02:16 -0500

    src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -142,3 +142,59 @@ pub use hypercore; // Re-export hypercore
 pub use message::Message;
 pub use protocol::{Command, CommandTx, DiscoveryKey, Event, Key, Protocol};
 pub use util::discovery_key;
+use std::error::Error;
+use std::fs;
+
+#[derive(Debug)]
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+
+    let contents = fs::read_to_string(config.file_path)?;
+
+        for line in search(&config.query, &contents) {
+            println!("{line}");
+        }
+    Ok(())
+}
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+let mut results = Vec::new();
+    for line in contents.lines() {
+        // do something with line
+        if line.contains(query) {
+            // do something with line
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
